989806d869a047f840be059f8356f22f813e7ccc
pankov <pankov@toowto.kz>
2023-03-03 14:36:07 +0600

    delete export // Object.defineProperty(exports, "__esModule", { value: true });

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Create a "close" button and append it to each list item
 const myNodelist = document.getElementsByTagName("LI");
 let i;
 for (i = 0; i < myNodelist.length; i++) {
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 // Click on a close button to hide the current list item
 const closeBtns = document.getElementsByClassName("close");
 for (i = 0; i < closeBtns.length; i++) {
-    let closeButton = closeBtns[i];
-    closeButton.addEventListener("click", () => {
-        const listItem = closeButton.parentElement;
-        if (!!listItem) {
-            listItem.style.display = "none";
-        }
-    });
+  let closeButton = closeBtns[i];
+  closeButton.addEventListener("click", () => {
+    const listItem = closeButton.parentElement;
+    if (!!listItem) {
+      listItem.style.display = "none";
+    }
+  });
 }
 // Add a "checked" symbol when clicking on a list item
-const list = document.querySelector('ul');
+const list = document.querySelector("ul");
 if (list) {
-    list.addEventListener('click', function (ev) {
-        let target = ev.target;
-        if (target) {
-            if (target.tagName === 'LI') {
-                target.classList.toggle('checked');
-            }
+  list.addEventListener(
+    "click",
+    function (ev) {
+      let target = ev.target;
+      if (target) {
+        if (target.tagName === "LI") {
+          target.classList.toggle("checked");
         }
-    }, false);
+      }
+    },
+    false
+  );
 }
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-    const li = document.createElement("li");
-    const input = document.getElementById("myInput");
-    if (!input)
-        return;
-    const inputValue = input.value;
-    const textNode = document.createTextNode(inputValue);
-    li.appendChild(textNode);
-    if (inputValue === '') {
-        alert("You must write something!");
+  const li = document.createElement("li");
+  const input = document.getElementById("myInput");
+  if (!input) return;
+  const inputValue = input.value;
+  const textNode = document.createTextNode(inputValue);
+  li.appendChild(textNode);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    const ul = document.getElementById("myUL");
+    if (ul) {
+      ul.appendChild(li);
     }
-    else {
-        const ul = document.getElementById("myUL");
-        if (ul) {
-            ul.appendChild(li);
-        }
+  }
+  input.value = "";
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  span.addEventListener("click", () => {
+    const listItem = span.parentElement;
+    if (!!listItem) {
+      listItem.style.display = "none";
     }
-    input.value = "";
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-    span.addEventListener("click", () => {
-        const listItem = span.parentElement;
-        if (!!listItem) {
-            listItem.style.display = "none";
-        }
-    });
+  });
 }
